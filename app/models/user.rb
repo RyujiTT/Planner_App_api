334549a5020d35af2979037c67fe7930f1d945ed
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-21 11:05:33 +0900

    [Add]Email_Custom_Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
   # bcrypt
   has_secure_password
 
@@ -8,6 +11,9 @@ class User < ApplicationRecord
                     maximum: 30, 
                     allow_blank: true 
                   }
+
+  validates :email, presence: true,
+                    email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   # \A     => 文字列の先頭にマッチ
@@ -22,5 +28,28 @@ class User < ApplicationRecord
                         allow_blank: true
                       },
                       allow_nil: true
+
+  ## methods
+  # class method  ###########################
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+  # class method end #########################
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 
 end
